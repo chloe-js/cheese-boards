070ebd7b6635c92208c1d916a3ef8111d8bf8e04
chloë js <91985648+chloe-js@users.noreply.github.com>
2022-11-03 17:32:43 +0000

    add cheese models and data types

---
+++ models/cheese.models.js
@@ -0,0 +1,20 @@
+const { Model, DataTypes } = require('sequelize')
+const db = require('../db/db')
+
+// many to many
+class Cheese extends Model { }
+
+Cheese.init({
+
+    title: {
+        type: DataTypes.STRING,
+    },
+    description: {
+        type: DataTypes.STRING,
+    },
+
+}, { 
+    sequelize: db 
+})
+
+module.exports = Cheese
