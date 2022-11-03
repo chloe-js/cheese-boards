ce548cdb6157f203c6404dd51e2ab71df030d528
chloë js <91985648+chloe-js@users.noreply.github.com>
2022-11-03 17:31:35 +0000

    add BOARD models and data types

---
+++ models/board.models.js
@@ -0,0 +1,23 @@
+const { Model, DataTypes } = require('sequelize')
+const db = require('../db/db')
+
+// many to many
+class Board extends Model { }
+
+Board.init({
+
+    type: {
+        type: DataTypes.STRING,
+    },
+    description: {
+        type: DataTypes.STRING,
+    },
+    rating: {
+        type: DataTypes.NUMBER,
+    }
+
+}, { 
+    sequelize: db 
+})
+
+module.exports = Board
