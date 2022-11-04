9a538d3bc3e38dcdcd7b3a4de030a896be7ec666
chloë js <91985648+chloe-js@users.noreply.github.com>
2022-11-04 15:15:09 +0000

    add testing association of board and users

--- testing/association.test.js
+++
@@ -1,5 +0,0 @@
-// Board - User relationship is tested
-// Multiple boards can be added to a User
-
-// Board - Cheese relationship is tested
-// A Board can have many Cheeses, and a Cheese can be on many Boards
---
+++ testing/associationBoardUser.test.js
@@ -0,0 +1,29 @@
+// Board - User relationship is tested
+// Multiple boards can be added to a User
+
+// Board - Cheese relationship is tested
+// A Board can have many Cheeses, and a Cheese can be on many Boards
+const db = require('../db/db')
+const { Cheese, Board, User } = require('../models/index')
+
+describe('Users and boards...', () => {
+
+    test('Profile can be assigned', async () => {
+        const user1 = await User.create({
+            name: 'name1',
+            email: 'email1@email.com'
+        })
+        const board1 = await Board.create({
+            type: 'newBoard',
+            describe: 'new description',
+            rating: 3
+        })
+
+        await user1.addBoard(board1)
+        const userBoard = await user1.getBoards()
+        expect(userBoard[0].id).toEqual(board1.id)
+        await user1.destroy()
+        await board1.destroy()
+    })
+})
+
